3c718a252e6b817248e699e762460dbfa6f7d163
dezoox <levi1@windowslive.com>
2021-08-26 21:08:51 +0200

    End of Section 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+         : base(options)
+        {
+
+        }
+
+        //adatbázis tábla
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
