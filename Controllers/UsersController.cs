b09ae55d814901dbaae31c8c559fd83482127d7c
dezoox <levi1@windowslive.com>
2021-09-05 12:05:15 +0200

    End of Section 4

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using DatingApp.Data;
 using DatingApp.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,9 +10,7 @@ using System.Threading.Tasks;
 
 namespace DatingApp.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,7 +18,9 @@ namespace DatingApp.Controllers
             _context = context;
         }
 
+
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             // return _context.Users.ToListAsync().Result;
@@ -27,6 +28,7 @@ namespace DatingApp.Controllers
         }
 
         //api/users/3
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
